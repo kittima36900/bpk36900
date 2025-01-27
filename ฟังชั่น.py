619aa0fced58c619169a666d1ba0bc25595a5290
kittima36900 <aggmook.k@gmail.com>
2025-01-27 14:10:55 +0700

    Update ฟังชั่น.py

--- ฟังชั่น.py
+++ ฟังชั่น.py
@@ -1,3 +1,24 @@
 def add_numbers(a,b):
   return a+b
-print(add_nummber(10,20))
+print(add_nummber(a,b))
+
+
+def add_numbers(a, b):
+    return a + b
+
+
+def square_number(a):
+    return a ** 2
+
+
+def sum_of_squares(numbers):
+    return sum(x ** 2 for x in numbers)
+
+
+def concatenate_strings(str1, str2, str3):
+    return str1 + str2 + str3
+
+
+
+def apply_discount(price, discount):
+    return price - (price * discount / 100)
