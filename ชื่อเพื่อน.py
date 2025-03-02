04428cb21bac8c80ec9b60bb3f3efb78019fefb9
kittima36900 <aggmook.k@gmail.com>
2025-03-02 14:58:56 +0700

    จำนวนนักเรียนlen.py

---
+++ ชื่อเพื่อน.py
@@ -0,0 +1,8 @@
+m5 = ['boom','chin','mogun', 'ohm','nice','ice1','kaimook',
+      'owen', 'stang','bank','mint', 'baitong','bell','noey','pear',
+      'paan','pin','babe','shampoo', 'nijung','post','fifa','imi', 'saint','ying','koon','nin', 'lookin','monae','kanoon']
+
+
+count = len(m5)
+
+print("จำนวนนักเรียนทั้งหมดในรายชื่อ:", count)
