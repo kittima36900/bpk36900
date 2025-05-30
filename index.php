2bdafb202a83041f39de201752b6d16ef1cf0d39
kittima36900 <aggmook.k@gmail.com>
2025-05-30 16:01:23 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+       <meta charset="UIF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>document</title>
+
+    </head>
+
+<body> 
+    <h2><?php echo "กิจกรรมในชั้นเรียน" ?> </h2>
+    
+    <h3>
+
+
+
+
+
+
+
+
+
+
+
+
+    </h3>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
